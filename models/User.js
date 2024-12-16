6abe6d593518c935b7fe0128ed2ba951e1b423d6
gogigogigogi <136942815+gogigogigogi@users.noreply.github.com>
2024-12-16 17:38:39 +0900

    OSH/Product 테이블 name 타입 변경

--- models/User.js
+++ models/User.js
@@ -1,31 +1,32 @@
-const User = (sequelize, DataTypes) =>{
-    const user = sequelize.define(
-        "user",
-        {
-            user_id : {
-                type: DataTypes.INTEGER,
-                primaryKey : true,
-                allowNull : false,
-                autoIncrement : true,
-            },
-            email : {
-                type: DataTypes.STRING(63),
-                allowNull: false,
-            },
-            password : {
-                type: DataTypes.STRING(63),
-                allowNull : false, 
-            },
-            nickname : {
-                type: DataTypes.STRING(63),
-                allowNull: false,
-            },
-        }, {
-            freezeTableName : true,
-            timestamps: false,
-        },
-    );
-    return user;
-}
+const User = (sequelize, DataTypes) => {
+  const user = sequelize.define(
+    'user',
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      email: {
+        type: DataTypes.STRING(63),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(63),
+        allowNull: false,
+      },
+      nickname: {
+        type: DataTypes.STRING(63),
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+      timestamps: false,
+    }
+  );
+  return user;
+};
 
 module.exports = User;
