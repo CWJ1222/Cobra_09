f070dcd0ac8036ccc2fe33dacb737d171e101953
suyeon-dev <choilynne.dev@gmail.com>
2024-12-18 11:14:37 +0900

    feat:헤더,렌더링 작업, 로고 교체 - 헤더(검색창 추가, 스타일 적용, 로고 교체) - 렌더링 작업(임시 페이지 추가)

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -1,46 +1,59 @@
 const { Op } = require('sequelize');
-const {Category, Product, User, Order} = require('../models');
+const { Category, Product, User, Order } = require('../models');
 
-exports.main = (req, res) =>{
-    res.render('index');
+/* FE: 렌더링 */
+// 홈페이지
+exports.main = (req, res) => {
+  res.render('index', { title: '홈페이지', currentPage: 'home' });
+};
+// 구매 페이지
+exports.purchase = (req, res) => {
+  res.render('purchase', { title: '구매 페이지', currentPage: 'purchase' });
+};
+// 판매 페이지
+exports.sell = (req, res) => {
+  res.render('sell', { title: '판매 페이지', currentPage: 'sell' });
 };
 
 // GET /products
-exports.getAllProducts = async (req, res) =>{
-    try {
-        const target = 1; // 세션 user_id에서 받아올 것
-        // products에서 user_id에 해당하는거 모두 가져옴
-        const products = await Product.findAll({
-            where : {host_id : target}, // user_id == host_id 같은 의미!
-            attributes : ['name', 'deadline', 'max_quantity'],
-            
-            // include :[
-            //     {model : User, attributes: ['user_id']}
-            // ],
-        })
-        res.status(200).send({isSuccess: true, products});
-    }catch(err) {
-        console.log('err', err);
-        res.status(200).send({isSuccess: false, message : '공동 구매 내역을 조회하는 중 오류가 발생했습니다.'});
-    }
+exports.getAllProducts = async (req, res) => {
+  try {
+    const target = 1; // 세션 user_id에서 받아올 것
+    // products에서 user_id에 해당하는거 모두 가져옴
+    const products = await Product.findAll({
+      where: { host_id: target }, // user_id == host_id 같은 의미!
+      attributes: ['name', 'deadline', 'max_quantity'],
+
+      // include :[
+      //     {model : User, attributes: ['user_id']}
+      // ],
+    });
+    res.status(200).send({ isSuccess: true, products });
+  } catch (err) {
+    console.log('err', err);
+    res.status(200).send({
+      isSuccess: false,
+      message: '공동 구매 내역을 조회하는 중 오류가 발생했습니다.',
+    });
+  }
 };
 
 // GET /joins
-exports.getAllJoins = async (req, res) =>{
-    try {
-        const target = 1; // 세션 user_id에서 받아올 것
-        // order에서 user_id에 해당하는거 모두 가져옴
-        const orders = await Order.findAll({
-            where: {user_id : target}, // user_id == host_id 같은 의미!
-            attributes: ['quantity'],
-            include :[
-                {model : Product, attributes: ['name', 'host_id']}
-            ],
-        })
-        res.status(200).send({isSuccess: true, orders});
-
-    } catch(err) {
-        console.log('err', err);
-        res.status(500).send({isSuccess: false, message: '구매 내역을 조회하는 중 오류가 발생했습니다.'});
-    }
-}
+exports.getAllJoins = async (req, res) => {
+  try {
+    const target = 1; // 세션 user_id에서 받아올 것
+    // order에서 user_id에 해당하는거 모두 가져옴
+    const orders = await Order.findAll({
+      where: { user_id: target }, // user_id == host_id 같은 의미!
+      attributes: ['quantity'],
+      include: [{ model: Product, attributes: ['name', 'host_id'] }],
+    });
+    res.status(200).send({ isSuccess: true, orders });
+  } catch (err) {
+    console.log('err', err);
+    res.status(500).send({
+      isSuccess: false,
+      message: '구매 내역을 조회하는 중 오류가 발생했습니다.',
+    });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const controller = require('../controller/Cmain');
 const router = express.Router();
 
-router.get('/', controller.main);
+// FE: 메인페이지 뷰 테스트
+router.get('/', controller.main); //홈페이지
+router.get('/purchase', controller.purchase); // 구매 페이지
+router.get('/sell', controller.sell); //판매 페이지
+
 router.get('/products', controller.getAllProducts);
 router.get('/joins', controller.getAllJoins);
-
 
 module.exports = router;
